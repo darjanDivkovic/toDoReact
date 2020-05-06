895e1cd70c001945d943638c9402dbe6a8a56531
Darjan Divkovic <daarjan.tz@gmail.com>
2020-05-06 23:30:04 +0200

    Is rendering todoitems now, not styled

---
+++ src/components/ToDoItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ToDoItem(props) {
+    return (
+        <div>
+            <h2>{props.content}</h2>
+        </div>
+    )
+}
--- src/components/toDoList.js
+++ src/components/toDoList.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
 
+import ToDoItem from './ToDoItem';
+
 export default class toDoList extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            todos: [
+                {id: 1, content: 'Eat'},
+                {id: 2, content: 'Code'},
+                {id: 3, content: 'Sleep'},
+            ],
+        }
+    }
     render() {
         return (
             <div className='toDoListContainer'>
                <input type='text' placeholder='ADD TODO'/>
                <button>ADD TODO</button>
+               <div class='todoitems-container'>
+                    <ul>
+                        {
+                            this.state.todos.map((todo)=> {
+                            return <ToDoItem content={todo.content} />
+                            })
+                        }
+                    </ul>
+               </div>
             </div>
         )
     }
