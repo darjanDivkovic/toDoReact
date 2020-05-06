41da98240b04b70b45cf90cc1d81fc445becc94a
Darjan Divkovic <daarjan.tz@gmail.com>
2020-05-06 23:45:23 +0200

    Can now add todos

--- src/components/toDoList.js
+++ src/components/toDoList.js
@@ -11,14 +11,31 @@ export default class toDoList extends Component {
                 {id: 2, content: 'Code'},
                 {id: 3, content: 'Sleep'},
             ],
+
+            currentInput : '',
         }
+        this.handleInput = this.handleInput.bind(this);
+        this.handleAddTodo = this.handleAddTodo.bind(this);
     }
+
+    handleInput(e){
+        this.setState({[e.target.name] : e.target.value})
+    }
+
+    handleAddTodo(){
+        let newTodo = {id: 4, content: this.state.currentInput};
+        let newTodos = Object.assign([], this.state.todos).concat(newTodo);
+        
+        this.setState({todos: newTodos });
+        this.setState({currentInput : ''});
+    }
+
     render() {
         return (
             <div className='toDoListContainer'>
-               <input type='text' placeholder='ADD TODO'/>
-               <button>ADD TODO</button>
-               <div class='todoitems-container'>
+               <input type='text' onChange={this.handleInput} value={this.state.currentInput}placeholder='ADD TODO' name='currentInput'/>
+               <button onClick={this.handleAddTodo} >ADD TODO</button>
+               <div className='todoitems-container'>
                     <ul>
                         {
                             this.state.todos.map((todo)=> {
