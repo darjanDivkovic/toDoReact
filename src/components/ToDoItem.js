af7e67d6911b7a365ca0923afca0e11005a68f7c
Darjan Divkovic <daarjan.tz@gmail.com>
2020-05-06 23:32:27 +0200

    TodoItem styled

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ToDoItem(props) {
     return (
-        <div>
+        <div className='todoItem'>
             <h2>{props.content}</h2>
         </div>
     )
