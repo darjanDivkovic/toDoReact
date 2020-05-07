c722af3006dfa8f0e2da53bdf14e8cff60ebe9d3
Darjan Divkovic <daarjan.tz@gmail.com>
2020-05-07 19:41:03 +0200

    Edit now toggles edit input with placeholder matching todo content

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { Component } from "react";
 
-export default function ToDoItem(props) {
+export default class ToDoItem extends Component {
+  constructor(props){
+      super(props);
+      this.state = {
+          editToggler : true,
+      }
+      this.handleEdit = this.handleEdit.bind(this);
+  }
+  handleEdit(){
+      this.setState({editToggler: !this.state.editToggler});
+  }
+
+  render() {
+    let edit = null;
+    if(this.state.editToggler) {
+        edit = <input type='text' placeholder={this.props.content} className='edit-input'/>
+    }
+
     return (
-        <div className='todoItem-container'>
-            <h2 className='content'>{props.content}</h2>
-            <div className='todoItem-buttons-container'>
-            <button className='edit'>EDIT</button>
-            <button onClick={props.deleteTodo.bind(this, props.id)} className='xButton'>DELETE</button>
-            </div>
+      <div className="todoItem-container">
+        <h2 className="content">{this.props.content}</h2>
+        { edit }
+        <div className="todoItem-buttons-container">
+          <button onClick={this.handleEdit} className="edit">EDIT</button>
+          <button
+            onClick={this.props.deleteTodo.bind(this, this.props.id)}
+            className="xButton">
+            DELETE
+          </button>
         </div>
-    )
+      </div>
+    );
+  }
 }
