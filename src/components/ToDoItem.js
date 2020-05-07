0866d6cfd1f7c3602c566d53606f97640fb8895a
Darjan Divkovic <daarjan.tz@gmail.com>
2020-05-07 19:09:27 +0200

    Styled and created edit button

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -4,7 +4,10 @@ export default function ToDoItem(props) {
     return (
         <div className='todoItem-container'>
             <h2 className='content'>{props.content}</h2>
+            <div className='todoItem-buttons-container'>
+            <button className='edit'>EDIT</button>
             <button onClick={props.deleteTodo.bind(this, props.id)} className='xButton'>DELETE</button>
+            </div>
         </div>
     )
 }
