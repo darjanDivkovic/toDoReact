c7c26a86ebf92196d4202632b7c11a2008ee4739
Darjan Divkovic <daarjan.tz@gmail.com>
2020-05-07 11:00:58 +0200

    did some styling

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ToDoItem(props) {
     return (
         <div className='todoItem-container'>
-            <h2>{props.content}</h2>
+            <h2 className='content'>{props.content}</h2>
             <button onClick={props.deleteTodo.bind(this, props.id)} className='xButton'>DELETE</button>
         </div>
     )
