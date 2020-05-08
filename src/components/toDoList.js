282f5fc465327a1b7563443064745cf8dfc42314
Darjan Divkovic <daarjan.tz@gmail.com>
2020-05-08 14:02:51 +0200

    Functionalities done for now, refactor, implement grid and mobile responsivenes up next.

--- src/components/toDoList.js
+++ src/components/toDoList.js
@@ -1,89 +1,91 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import nextID from 'react-id-generator';
-import ToDoItem from './ToDoItem';
-
+import nextID from "react-id-generator";
+import ToDoItem from "./ToDoItem";
 
 export default class toDoList extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            todos: [
-                {id: nextID(), content: 'Eat'},
-                {id: nextID(), content: 'Code'},
-                {id: nextID(), content: 'Sleep'},
-            ],
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { id: nextID(), content: "Eat" },
+        { id: nextID(), content: "Code" },
+        { id: nextID(), content: "Sleep" },
+      ],
 
-            currentInput : '',
-            editInput : '',
-        }
-        this.handleInput = this.handleInput.bind(this);
-        this.handleAddTodo = this.handleAddTodo.bind(this);
-        this.handleDeleteTodo = this.handleDeleteTodo.bind(this);
-        this.saveEdit = this.saveEdit.bind(this);
-    }
+      currentInput: "",
+      editInput: "",
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.handleAddTodo = this.handleAddTodo.bind(this);
+    this.handleDeleteTodo = this.handleDeleteTodo.bind(this);
+    this.saveEdit = this.saveEdit.bind(this);
+  }
 
-    handleInput(e){
-        this.setState({[e.target.name] : e.target.value})
-    }
+  handleInput(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
-    handleAddTodo(){
-        let newTodo = {id: nextID(), content: this.state.currentInput};
-        let newTodos = Object.assign([], this.state.todos).concat(newTodo);
-        
-        this.setState({todos: newTodos });
-        this.setState({currentInput : ''});
-    }
+  handleAddTodo() {
+    let newTodo = { id: nextID(), content: this.state.currentInput };
+    let newTodos = Object.assign([], this.state.todos).concat(newTodo);
 
-    handleDeleteTodo(todoToDelete){
-        console.log(todoToDelete);
-        let newTodos = this.state.todos.filter((todo) => {
-            if(todo.id === todoToDelete) return false;
-            else return true;
-        })
-        this.setState({todos : newTodos})
-    }
+    this.setState({ todos: newTodos });
+    this.setState({ currentInput: "" });
+  }
 
-    saveEdit(idToChangeContent){
-        // Take id and in todos change content of obj with that id
-        console.log(idToChangeContent);
-        
-        let newTodos = this.state.todos.map((todo)=> {
-            if(todo.id === idToChangeContent && this.state.editInput !== '') {
-                todo.content = this.state.editInput;
-                return todo;
-            }
-            else return todo;
-        })
+  handleDeleteTodo(todoToDelete) {
+    console.log(todoToDelete);
+    let newTodos = this.state.todos.filter((todo) => {
+      if (todo.id === todoToDelete) return false;
+      else return true;
+    });
+    this.setState({ todos: newTodos });
+  }
 
-       
-        // Set new state
-        this.setState({todos : newTodos});
-        // clear edit input
-        this.setState({editInput : ''}); 
-        
-    }
+  saveEdit(idToChangeContent) {
+    // Take id and in todos change content of obj with that id
+    let newTodos = this.state.todos.map((todo) => {
+      if (todo.id === idToChangeContent && this.state.editInput !== "") {
+        todo.content = this.state.editInput;
+        return todo;
+      } else return todo;
+    });
 
-    render() {
-        return (
-            <div className='toDoListContainer'>
-               <input type='text' onChange={this.handleInput} value={this.state.currentInput} placeholder='ADD TODO' name='currentInput'/>
-               <button onClick={this.handleAddTodo} >ADD TODO</button>
-               <div className='todoitems-container'>
-                    <ul>
-                        {
-                            this.state.todos.map((todo)=> {
-                            return <ToDoItem key={todo.id} 
-                                             id={todo.id}
-                                             content={todo.content} 
-                                             deleteTodo={this.handleDeleteTodo}
-                                             handleInput={this.handleInput}
-                                             saveEdit={this.saveEdit}/>
-                            })
-                        }
-                    </ul>
-               </div>
-            </div>
-        )
-    }
+    // Set new state
+    this.setState({ todos: newTodos });
+    // clear edit input
+    this.setState({ editInput: "" });
+  }
+
+  render() {
+    return (
+      <div className="toDoListContainer">
+        <input
+          type="text"
+          onChange={this.handleInput}
+          value={this.state.currentInput}
+          placeholder="ADD TODO"
+          name="currentInput"
+        />
+        <button onClick={this.handleAddTodo}>ADD TODO</button>
+        <div className="todoitems-container">
+          <ul>
+            {this.state.todos.map((todo) => {
+              return (
+                <ToDoItem
+                  key={todo.id}
+                  id={todo.id}
+                  content={todo.content}
+                  deleteTodo={this.handleDeleteTodo}
+                  handleInput={this.handleInput}
+                  saveEdit={this.saveEdit}
+                />
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
