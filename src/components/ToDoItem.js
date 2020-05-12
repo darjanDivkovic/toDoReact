e35bdad8e9ba5e368c105abff642aae582decd19
Darjan Divkovic <daarjan.tz@gmail.com>
2020-05-12 20:05:34 +0200

    Styled checkButton

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -18,11 +18,14 @@ export default class ToDoItem extends Component {
   completeTodo(completedID, content) {
     let date = new Date();
 
-    console.log(completedID);
     // delete it from state here
     this.props.deleteTodo(completedID);
 
-    let completedTodo = { id : completedID, content : content, completedTime : date.toLocaleTimeString()};
+    let completedTodo = {
+      id: completedID,
+      content: content,
+      completedTime: date.toLocaleTimeString(),
+    };
 
     // Add it to completed todos
     this.props.addToCompletedTodos(completedTodo);
@@ -31,21 +34,31 @@ export default class ToDoItem extends Component {
   render() {
     let edit;
     if (this.state.editToggler) {
-      edit = <EditorToDo placeholder={this.props.content}
-                         handleInput={this.props.handleInput}
-                         toggleEdit={this.toggleEdit}
-                         id={this.props.id} 
-                         saveEdit={this.props.saveEdit}/>;
+      edit = (
+        <EditorToDo
+          placeholder={this.props.content}
+          handleInput={this.props.handleInput}
+          toggleEdit={this.toggleEdit}
+          id={this.props.id}
+          saveEdit={this.props.saveEdit}
+        />
+      );
     }
 
     return (
       <li className="todo-item-container">
-        <div className='content-container'>
-        <button onClick={this.completeTodo.bind(this, this.props.id, this.props.content)}>
-        ✔
-        </button>
-        <h2 className="content">{this.props.content}</h2>
-        {edit}
+        <div className="content-container">
+          <button className="checkButton"
+            onClick={this.completeTodo.bind(
+              this,
+              this.props.id,
+              this.props.content
+            )}
+          >
+            ✔
+          </button>
+          <h2 className="content">{this.props.content}</h2>
+          {edit}
         </div>
         <div className="todo-item-buttons-container">
           <button onClick={this.toggleEdit} className="edit">
@@ -58,7 +71,7 @@ export default class ToDoItem extends Component {
             delete
           </button>
         </div>
-        </li>
+      </li>
     );
   }
 }
